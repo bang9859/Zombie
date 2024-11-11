ae86b4d985c3d12126130a31650aa37cb4f7b806
bang9859 <kokozz9859@gmail.com>
2024-11-11 15:44:19 +0900

    Feat Constructor and variable

--- zombie/src/zombie/Unit.java
+++ zombie/src/zombie/Unit.java
@@ -1,5 +1,15 @@
 package zombie;
 
-public class Unit {
-
+abstract public class Unit {
+	protected int pos;
+	protected int hp;
+	protected int maxPower;
+	protected Unit(int pos, int hp, int maxPower){
+		this.pos = pos;
+		this.hp = hp;
+		this.maxPower = maxPower;
+	}
+	public void Attack() {
+		
+	}
 }
