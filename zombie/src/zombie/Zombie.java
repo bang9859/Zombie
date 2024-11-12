ee1f2211a71ddb9d37ae0f928626115300466427
bang9859 <kokozz9859@gmail.com>
2024-11-12 10:36:18 +0900

    Feat interface method at Zombie.java

--- zombie/src/zombie/Zombie.java
+++ zombie/src/zombie/Zombie.java
@@ -2,7 +2,7 @@ package zombie;
 
 import java.util.Random;
 
-public class Zombie extends Unit {
+public class Zombie extends Unit implements Regenable{
 
 	protected Zombie(int pos, int hp, int maxPower) {
 		super(pos, hp, maxPower);
@@ -11,12 +11,19 @@ public class Zombie extends Unit {
 	@Override
 	public void Attack(Unit target) {
 
-		int power = ZombieGame.ran.nextInt(getMaxPower());
+		int power = ZombieGame.ran.nextInt(getMaxPower())+1;
 		if (target.getHp() - power < 0) {
 			target.setHp(-target.getHp());
 		} else {
 			target.setHp(-power);
 		}
+	}
+
+	@Override
+	public void Regenable() {
+		int randomHeal = ZombieGame.ran.nextInt(20)+1;
+		setHp(randomHeal);
+		System.out.println("피를 자동으로 회복하였습니다.");
 	}
 
 }
