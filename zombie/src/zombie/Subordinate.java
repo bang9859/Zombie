07e16b7dfdd645bb7240ef8a56b36d07886c4e34
bang9859 <kokozz9859@gmail.com>
2024-11-12 12:49:47 +0900

    fix regenHp method

--- zombie/src/zombie/Subordinate.java
+++ zombie/src/zombie/Subordinate.java
@@ -8,7 +8,7 @@ public class Subordinate extends Zombie implements Regenable{
 
 	@Override
 	public void regenHp() {
-		int randomHeal = ZombieGame.ran.nextInt(getHp())+1;
+		int randomHeal = ZombieGame.ran.nextInt(20)+1;
 		setHp(randomHeal);
 		System.out.printf("부하 좀비가 피를 %d만큼 회복하였습니다. 현재 부하좀비 HP: %d\n",randomHeal,getHp());
 	}
