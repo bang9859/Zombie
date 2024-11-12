15db874314b6fd88b18e5425544c1b7067350dda
bang9859 <kokozz9859@gmail.com>
2024-11-12 12:15:36 +0900

    Update attack method at Zombie Java

--- zombie/src/zombie/Zombie.java
+++ zombie/src/zombie/Zombie.java
@@ -1,7 +1,5 @@
 package zombie;
 
-import java.util.Random;
-
 public class Zombie extends Unit {
 
 	protected Zombie(int pos, int hp, int maxPower) {
@@ -11,14 +9,15 @@ public class Zombie extends Unit {
 	@Override
 	protected void attack(Unit target) {
 
-		int power = ZombieGame.ran.nextInt(getMaxPower())+1;
+		int power = ZombieGame.ran.nextInt(getMaxPower()) + 1;
+		int currentHeroHp = target.getHp();
 		if (target.getHp() - power < 0) {
 			target.setHp(-target.getHp());
 		} else {
 			target.setHp(-power);
 		}
+		String message = String.format("퍽 [%d(-%d)/%d]", currentHeroHp, power, target.getHp());
+		System.out.println(message);
 	}
-
-	
 
 }
